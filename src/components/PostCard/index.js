0eebb404334f6bfd8a8092f237a0ce85eaab79c3
SylphidZoul <contact@jcruzcardozo.com>
2021-02-26 00:02:41 -0300

    Fixed a edit redirect bug at PostCard

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -7,7 +7,7 @@ export const PostCard = ({ postData, onDelete }) => {
   const history = useHistory()
 
   const handleEdit = () => {
-    history.push(`/posts/edit/${postData.id}`, postData)
+    history.push(`/posts/edit/${postData.id}`, { postData })
   }
 
   return (
