a7c090198cac5158b5b63b638604d3151af7eba0
SylphidZoul <contact@jcruzcardozo.com>
2021-02-26 04:08:18 -0300

    Added FadeIn animation to Layout

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,11 +1,20 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Colors } from '../../styles/colors'
+
+const FadeIn = keyframes`
+  from {
+    opacity: 0;
+  } to {
+    opacity: 1;
+  }
+`
 
 export const Div = styled.div`
   width: 100%;
   max-width: 1024px;
   padding: 0 24px 48px;
   margin: 0 auto;
+  animation: ${FadeIn} 500ms ease-in forwards;
 `
 
 export const Header = styled.header`
