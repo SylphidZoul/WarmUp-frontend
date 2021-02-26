c620faf4e35bc114678c336896f4438f50c2eefb
SylphidZoul <contact@jcruzcardozo.com>
2021-02-25 23:33:51 -0300

    Integrated Loading toast and Error toast in Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { SpinnerToast } from '../SpinnerToast'
+import { ErrorToast } from '../ErrorToast'
 import { Div, Header, H2 } from './styles'
 import PropTypes from 'prop-types'
 
-export const Layout = ({ title, children }) => {
+export const Layout = ({ title, children, isFetching = false, error = '' }) => {
   return (
     <Div>
       <Header>
         <H2>{title}</H2>
       </Header>
+      <SpinnerToast isFetching={isFetching} />
+      <ErrorToast errorMessage={error} />
       {children}
     </Div>
   )
 }
 
 Layout.propTypes = {
-  title: PropTypes.string,
-  children: PropTypes.node
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  isFetching: PropTypes.bool,
+  error: PropTypes.string
 }
