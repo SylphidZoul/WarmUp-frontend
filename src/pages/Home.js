8f428a3129bd86774f6795850080e279c65b16b7
SylphidZoul <contact@jcruzcardozo.com>
2021-02-25 23:49:46 -0300

    Finished Home's page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PostsContext } from '../context/PostsContext'
 import { Layout } from '../components/Layout'
+import { PostsList } from '../components/PostsList'
 
 const Home = () => {
+  const { posts, isFetching, error, deletePost } = useContext(PostsContext)
+
   return (
-    <Layout title='All posts'>
-      Home
+    <Layout
+      title='All posts'
+      isFetching={isFetching}
+      error={error}
+    >
+      <PostsList
+        posts={posts}
+        onDelete={deletePost}
+      />
     </Layout>
   )
 }
