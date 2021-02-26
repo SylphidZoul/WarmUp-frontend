050023ea4d9c59216919382d063bf6016b02eb2c
SylphidZoul <contact@jcruzcardozo.com>
2021-02-26 04:04:02 -0300

    Finished EditPost Page

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PostsContext } from '../context/PostsContext'
 import { Layout } from '../components/Layout'
+import { PostForm } from '../components/PostForm'
 
-const EditPost = () => {
+const EditPost = ({ location, match }) => {
+  const { isFetching, error, updatePost } = useContext(PostsContext)
+
+  const handleEdit = (changes) => {
+    const updates = { ...location.state.currentPost, changes }
+    updatePost(match.params.id, updates)
+  }
+
+  if (!location.state) {
+    return (
+      <Layout title={`The post with id ${match.params.id} was not found o cannot be edited.`} />
+    )
+  }
+
   return (
-    <Layout title='Edit the current post'>
-      EditPost
+    <Layout
+      isFetching={isFetching}
+      error={error}
+    >
+      <PostForm
+        formTitle={`Editing post ${match.params.id}`}
+        onSubmit={handleEdit}
+        postToEdit={location.state.currentPost}
+      />
     </Layout>
   )
 }
